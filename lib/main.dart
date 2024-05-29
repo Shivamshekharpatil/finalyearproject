eba6fd80b0e76226031bc9bb564de5cfe2e91e51
shivampatil <patilshivam2617@gmail.com>
2024-05-29 15:50:10 +0100

    Created SplashScreen and made some minor changes in it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:finalyearproject/screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Text('Hello')),
+      home: SplashScreen(),
     );
   }
 }
