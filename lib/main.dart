ede434e67d1b072001eccbe0d37815c803a019c6
shivampatil <patilshivam2617@gmail.com>
2024-06-07 01:18:12 +0100

    Tried to establish connection between app and firebase. Also added some packages related to firebase in pubspec yaml file and Updated Main dart file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:finalyearproject/controllers/authentication_controller.dart';
 import 'package:finalyearproject/screen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
-void main()
+void main() async
 {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp().then((value){
+    Get.put(AuthenticationController());
+  });
+
   Get.put(AuthenticationController());
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   SystemChrome.setPreferredOrientations(
