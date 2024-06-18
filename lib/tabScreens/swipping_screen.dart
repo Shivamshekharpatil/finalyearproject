0aba8cd5fb2f53c32e060ff61152f1bb13434329
shivampatil <patilshivam2617@gmail.com>
2024-06-18 21:56:31 +0100

    Swipe feature added, Now user can able to swipe.

--- lib/tabScreens/swipping_screen.dart
+++ lib/tabScreens/swipping_screen.dart
@@ -1,4 +1,6 @@
+import 'package:finalyearproject/controllers/profile-controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SwippingScreen extends StatefulWidget {
   const SwippingScreen({super.key});
@@ -7,19 +9,36 @@ class SwippingScreen extends StatefulWidget {
   State<SwippingScreen> createState() => _SwippingScreenState();
 }
 
-class _SwippingScreenState extends State<SwippingScreen> {
+class _SwippingScreenState extends State<SwippingScreen>
+{
+  ProfileController profileController = Get.put(ProfileController());
+
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Swipping Screen",
-          style: TextStyle(
-            color: Colors.green,
-            fontSize: 20,
-          ),
-        ),
-      ),
+    return Scaffold(
+      body: Obx(()
+    {
+      return PageView.builder(
+          itemCount: profileController.allUsersProfileList.length,
+          controller: PageController(initialPage: 0, viewportFraction: 1),
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index)
+          {
+            final eachProfileInfo = profileController.allUsersProfileList[index];
+
+            return DecoratedBox(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        eachProfileInfo.imageProfile.toString(),
+                      ),
+                      fit: BoxFit.cover,
+                    )
+                ));
+          }
+      );
+    }),
     );
   }
 }
