b04e6383564cd221afd51f6271cf7f1369c8b63d
shivampatil <patilshivam2617@gmail.com>
2024-06-19 14:06:06 +0100

    Swipe screen designed and added some info to present the user on screen.

--- lib/tabScreens/swipping_screen.dart
+++ lib/tabScreens/swipping_screen.dart
@@ -9,37 +9,162 @@ class SwippingScreen extends StatefulWidget {
   State<SwippingScreen> createState() => _SwippingScreenState();
 }
 
-class _SwippingScreenState extends State<SwippingScreen>
-{
+class _SwippingScreenState extends State<SwippingScreen> {
   ProfileController profileController = Get.put(ProfileController());
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Obx(()
-    {
-      return PageView.builder(
-          itemCount: profileController.allUsersProfileList.length,
-          controller: PageController(initialPage: 0, viewportFraction: 1),
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index)
-          {
-            final eachProfileInfo = profileController.allUsersProfileList[index];
+      body: Obx(() {
+        return PageView.builder(
+            itemCount: profileController.allUsersProfileList.length,
+            controller: PageController(initialPage: 0, viewportFraction: 1),
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              final eachProfileInfo =
+                  profileController.allUsersProfileList[index];
 
-            return DecoratedBox(
+              return DecoratedBox(
                 decoration: BoxDecoration(
                     image: DecorationImage(
-                      image: NetworkImage(
-                        eachProfileInfo.imageProfile.toString(),
+                  image: NetworkImage(
+                    eachProfileInfo.imageProfile.toString(),
+                  ),
+                  fit: BoxFit.cover,
+                )),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+
+                      //filter icon button
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 8),
+                          child: IconButton(
+                            onPressed: ()
+                            {
+
+                            },
+                            icon: const Icon(
+                              Icons.filter_list,
+                              size: 30,
+                            ),
+                          ),
+                        ),
                       ),
-                      fit: BoxFit.cover,
-                    )
+
+                      const Spacer(),
+
+                      //user data
+                      GestureDetector(
+                        onTap: () {},
+                        child: Column(
+                          children: [
+                            //name
+                            Text(
+                              eachProfileInfo.name.toString(),
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                letterSpacing: 4,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+
+                            //age - city
+                            Text(
+                              eachProfileInfo.age.toString() + " ⦾ " + eachProfileInfo.city.toString(),
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                letterSpacing: 4,
+                              ),
+                            ),
+
+                            const SizedBox(
+                              height: 4,
+                            ),
+
+                            //Jobtitle and education
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.white30,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(16)),
+                                  ),
+                                  child: Text(
+                                    eachProfileInfo.jobtitle.toString(),
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+
+                            //country and nationality
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.white30,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                        BorderRadius.circular(16)),
+                                  ),
+                                  child: Text(
+                                    eachProfileInfo.country.toString(),
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+
+                                const SizedBox(
+                                  width: 6,
+                                ),
+
+                                //jobtitle
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.white30,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                        BorderRadius.circular(16)),
+                                  ),
+                                  child: Text(
+                                    eachProfileInfo.nationality.toString(),
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+
+
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-            );
-          }
-      );
-    }),
+              );
+            });
+      }),
     );
   }
 }
